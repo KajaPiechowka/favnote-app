0097bf5ae027df76b6cea64cf38df37cbf5ccd21
Kaja Piechówka <kaja.piechowka@gmail.com>
2021-02-22 15:09:19 +0100

    atoms and card component styling

--- favnote/src/components/atoms/Button/Button.js
+++ favnote/src/components/atoms/Button/Button.js
@@ -2,19 +2,21 @@ import styled, { css } from 'styled-components';
 
 const Button = styled.button`
   padding: 0;
-  background-color: ${({theme})=>theme.primary};
+  background-color: ${({ theme }) => theme.primary};
   width: 220px;
   height: 47px;
   border: none;
+  outline: none;
+  cursor: pointer;
   border-radius: 50px;
   text-transform: uppercase;
-  font-weight: ${({theme})=>theme.bold};
+  font-weight: ${({ theme }) => theme.bold};
   font-size: 16px
     ${({ secondary }) =>
       secondary &&
       css`
         border: none;
-        background-color: ${({theme})=>theme.grey200};
+        background-color: ${({ theme }) => theme.grey200};
         width: 105px;
         height: 30px;
         font-size: 10px;
---
+++ favnote/src/components/atoms/ButtonIcon/ButtonIcon.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+const ButtonIcon = styled.button`
+width:67px;
+height:67px;
+border-radius:20px;
+background-image: url(${({icon})=>icon});
+background-repeat: no-repeat;
+background-position: 50% 50%;
+background-size: 40%;
+border: none;
+outline:none;
+background-color: transparent;
+cursor: pointer;
+:active{
+background-color: white;
+outline: none;
+border: none;
+}
+
+
+`
+
+
+export default ButtonIcon
