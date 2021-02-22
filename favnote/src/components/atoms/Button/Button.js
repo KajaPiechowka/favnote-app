4e87bfb637628666c4e77dbaaff2304b18dbc3ee
Kaja Piechówka <kaja.piechowka@gmail.com>
2021-02-22 13:23:09 +0100

    main theme and atomic design folders done

---
+++ favnote/src/components/atoms/Button/Button.js
@@ -0,0 +1,24 @@
+import styled, { css } from 'styled-components';
+
+const Button = styled.button`
+  padding: 0;
+  background-color: ${({theme})=>theme.primary};
+  width: 220px;
+  height: 47px;
+  border: none;
+  border-radius: 50px;
+  text-transform: uppercase;
+  font-weight: ${({theme})=>theme.bold};
+  font-size: 16px
+    ${({ secondary }) =>
+      secondary &&
+      css`
+        border: none;
+        background-color: ${({theme})=>theme.grey200};
+        width: 105px;
+        height: 30px;
+        font-size: 10px;
+      `};
+`;
+
+export default Button;
